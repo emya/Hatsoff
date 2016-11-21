b9564914abffd26e0ddc087462255c799ec641c6
ayadaemi <e.ayada810004@gmail.com>
2016-11-22 08:22:12 +0900

    Added some P0 tasks

--- nodejs/message.js
+++ nodejs/message.js
@@ -564,6 +564,7 @@ io.on('connection', function(socket){
 
     HatsoffPost.find({'to_uid':socket.uid}).count(function(err, count){
       if (err) throw err;
+      console.log("count:"+count);
       socket.emit('number hatsoff', count);
     }); 
 
@@ -597,26 +598,26 @@ io.on('connection', function(socket){
                socket.emit('update timeline history', {share:newdocs, community:communitydocs});
            }else{
 
-		   async.each(sharedocs, function(docs){
-			  if (docs.content_type == 1){
-			  async.waterfall([
-			     function(callback){
-				      CommunityPost.findOne({'_id':docs.content_id}).exec(function(err, post){
-				        callback(null, post);
-				      });
-			     },
-			     function(post){
-    				 console.log("second");
-    				 docs.set('content', post.toJSON(), {strict: false});
-    				 newdocs.push(docs);
-    				 //console.log("****newdocs****:"+newdocs);
-    				 curIdx += 1;
-    				 if (curIdx == len){
-    				  console.log("***********************************length of newdocs:"+newdocs.length);
-    				  socket.emit('update timeline history', {share:newdocs, community:communitydocs});
-    				 }
-    			 }
-    		], function(err, result){
+    		   async.each(sharedocs, function(docs){
+    			  if (docs.content_type == 1){
+    			  async.waterfall([
+    			     function(callback){
+    				      CommunityPost.findOne({'_id':docs.content_id}).exec(function(err, post){
+    				        callback(null, post);
+    				      });
+    			     },
+    			     function(post){
+        				 console.log("second");
+        				 docs.set('content', post.toJSON(), {strict: false});
+        				 newdocs.push(docs);
+        				 //console.log("****newdocs****:"+newdocs);
+        				 curIdx += 1;
+        				 if (curIdx == len){
+        				  console.log("***********************************length of newdocs:"+newdocs.length);
+        				  socket.emit('update timeline history', {share:newdocs, community:communitydocs});
+        				 }
+        			 }
+        		], function(err, result){
     	});
 
 			} else if(docs.content_type == 2){

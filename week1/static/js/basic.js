111309a0b3fc8f1a1a1ab0eab7f22cf49091b6bc
streetwitness <streetwitness@gmail.com>
2017-06-26 20:03:54 +0900

    Fixed bug hat image file name

--- week1/static/js/basic.js
+++ week1/static/js/basic.js
@@ -88,7 +88,7 @@ function unhatsoffCommunityFunction(uid, c_id){
     //popHatsoff();
     socket.emit('give unhatsoff', {to_uid:uid, uid:socket.uid, content_type:1, content_id:c_id});
     var hatsoff = document.getElementById("hatsoff_"+c_id);
-    hatsoff.innerHTML = '<img src="/static/inactive_grey_hat_32x32.png" class="icons-img" onclick="hatsoffCommunityFunction(\''+uid+'\',\''+c_id+'\')">';
+    hatsoff.innerHTML = '<img src="/static/images/inactive_grey_hat_32x32.png" class="icons-img" onclick="hatsoffCommunityFunction(\''+uid+'\',\''+c_id+'\')">';
 }
 
 function addCommunityFunction(uid){

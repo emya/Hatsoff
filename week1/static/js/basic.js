2f7ab8e96027aea6fa53220551baa2e99a1e9186
emya <e.ayada810004@gmail.com>
2017-11-15 22:16:04 -0800

    initial commit for new community page (#74)

--- week1/static/js/basic.js
+++ week1/static/js/basic.js
@@ -3,16 +3,18 @@ var socket = io.connect('http://localhost:8889');
 //var socket = io.connect('http://ec2-35-164-130-35.us-west-2.compute.amazonaws.com:8889');
 console.log("socket :", socket);
 
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 var cmembers = localStorage.getItem("num-cmembers");
 if (cmembers == null){
 	socket.emit('get community members number');
 }else{
-	document.getElementById('num-cmembers').innerHTML = cmembers;
+	//document.getElementById('num-cmembers').innerHTML = cmembers;
 }
 
 socket.on('community members number', function(len){
     console.log("length :", len);
-    document.getElementById('num-cmembers').innerHTML = len;
+    //document.getElementById('num-cmembers').innerHTML = len;
     localStorage.setItem("num-cmembers", len); 
     var cmembers = localStorage.getItem("num-cmembers");
     console.log(cmembers);
@@ -49,7 +51,6 @@ function logoutFunction() {
     localStorage.removeItem("num-cmembers");
 }
 
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 function sharePortfolioFunction(uid, c_id){
     console.log('share portfolio function');
